5139486fb4e3222aedfa71d9ff21871dcc81bc13
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2020-12-21 09:25:24 +0300

    updated template name

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
     const { createPage } = actions
   
-    const blogPostTemplate = require.resolve(`./src/templates/BlogTemplate.js`)
+    const blogPostTemplate = require.resolve(`./src/templates/MarkdownTemplate.js`)
   
     const result = await graphql(`
       {
